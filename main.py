ca6647487a229bb1df5bd3c0a6c87b88642642e0
ayllie <ayllie@pm.me>
2024-09-02 18:06:08 +0100

    Player rotation

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
+import player
+import circleshape
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    ingame_player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        ingame_player.update(dt)
+        ingame_player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
